918bc63325ebab3b249b3fe83de7b557af37bb37
Gordon Tyler <gordon.tyler@synaptivemedical.com>
2021-12-20 16:17:23 -0500

    Update read_input skeleton to read from filename passed as arg.

--- skeleton/src/main.rs
+++ skeleton/src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+use std::fs::File;
 use std::io::prelude::*;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
@@ -12,7 +14,12 @@ fn main() -> Result<()> {
 
 fn read_input() -> Result<Vec<String>> {
     let mut input = String::new();
-    std::io::stdin().read_to_string(&mut input)?;
+    if let Some(filename) = env::args().skip(1).nth(0) {
+        let mut r = File::open(filename)?;
+        r.read_to_string(&mut input)?;
+    } else {
+        std::io::stdin().read_to_string(&mut input)?;
+    }
 
     Ok(input.lines().map(|l| l.to_string()).collect())
 }
